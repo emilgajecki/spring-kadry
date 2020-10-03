d89e7868eafe7c6e12b02fc470d8c5a3d2a4e866
emilgajecki <60809250+emilgajecki@users.noreply.github.com>
2020-10-03 10:08:54 +0200

    New Spring files

---
+++ src/main/java/com/example/kadry2/Kadry2Application.java
@@ -0,0 +1,13 @@
+package com.example.kadry2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kadry2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kadry2Application.class, args);
+	}
+
+}
